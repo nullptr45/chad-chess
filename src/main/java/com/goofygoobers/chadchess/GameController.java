794fc4f874c8e894043d83c65580cecacf2fe2ad
nullptr45 <papirakisnicolas00@gmail.com>
2023-02-10 10:52:31 -0800

    fixed stupid ass merge conflict

--- src/main/java/com/goofygoobers/chadchess/GameController.java
+++ src/main/java/com/goofygoobers/chadchess/GameController.java
@@ -2,21 +2,10 @@ package com.goofygoobers.chadchess;
 import org.springframework.web.bind.annotation.*;
 
 import com.goofygoobers.chadchess.logic.*;
-
 /**
  * GameController
  * The GameController class is in charge of handle HTTP requests from the client
  */
 @RestController
 public class GameController {
-    private Board board;
-
-    GameController() {
-        board = new Board();
-    }
-
-    @GetMapping("/test")
-    public @ResponseBody String test(@RequestParam("x") int x, @RequestParam("y") int y) {
-        return board.getTileAt(x, y).toString();
-    }
 }
--- src/main/java/com/goofygoobers/chadchess/logic/pieces/King.java
+++ src/main/java/com/goofygoobers/chadchess/logic/pieces/King.java
@@ -1,6 +1,5 @@
 package com.goofygoobers.chadchess.logic.pieces;
 
-<<<<<<< HEAD
 import com.goofygoobers.chadchess.logic.Color;
 import com.goofygoobers.chadchess.logic.V2;
 
@@ -46,45 +45,14 @@ public class King extends Piece{
 
     public boolean goesDiagonally() {
         return false;
-=======
-import com.goofygoobers.chadchess.logic.Board;
-import com.goofygoobers.chadchess.logic.Piece;
-import com.goofygoobers.chadchess.logic.Tile;
-
-public class King extends Piece {
-    public King(boolean firstTeam) {
-        super(firstTeam);
->>>>>>> 78f7754daf9d08f4025f9deb0a7908c73502a144
     }
 
     @Override
     public String toString() {
-<<<<<<< HEAD
         if(COLOR == Color.WHITE) {
             return "♔︎";
         } else {
             return "♚︎";
         }
     }
-=======
-        return "King";
-    }
-
-    @Override
-    public boolean isMoveValid(Board board, Tile start, Tile end) {
-        if (end.getPiece().isFirstTeam() == this.isFirstTeam()) {
-            return false;
-        }
-
-        int x = Math.abs(start.getX() - end.getX());
-        int y = Math.abs(start.getY() - end.getY());
-
-        if (x + y == 1) {
-            return true;
-        }
-
-        return false;
-    }
-    
->>>>>>> 78f7754daf9d08f4025f9deb0a7908c73502a144
 }
