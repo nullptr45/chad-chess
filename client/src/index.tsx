6db1ca35a9cb030569b5d595cc3940bb32233f20
nullptr45 <papirakisnicolas00@gmail.com>
2023-02-09 18:54:30 -0800

    chess board ui

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Game from "./components/game";
+
+import "./style/style.css"
+
+ReactDOM.render(
+    <div>
+        <Game></Game>
+    </div>,
+    document.getElementById("root")
+);
---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
