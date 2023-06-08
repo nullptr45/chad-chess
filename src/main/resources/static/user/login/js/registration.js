da10159c213311893ed99fc9485577c175a38c8e
Peter Sokolov <2018278@apps.nsd.org>
2023-06-08 11:03:41 -0700

    a

---
+++ src/main/resources/static/user/login/js/registration.js
@@ -0,0 +1,22 @@
+var ui = {
+    inputs: {
+        username: document.getElementById("username"),
+        password: document.getElementById("password"),
+    },
+    buttons: {
+        signUp: document.getElementById("sign-up"),
+    }
+}
+
+ui.buttons.signUp.addEventListener("click", (ev) => {
+    var username = ui.inputs.username.value;
+    var password = ui.inputs.password.value;
+    
+    //build and send request to sign up
+    const id = await (await fetch(`/register-user?username=${username}&password=${password}`)).json()
+    window.location.href = `${window.location.origin}/lobby/index.html;
+
+});
+
+
+
